f0ba0cabb971c135aa7d83e06d60e4634469ade0
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-18 00:14:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -16,12 +16,12 @@ def directors_totals(nds)
     total = 0 
     j = 0 
     while j < nds[i][:movies].length do
-      total += nds[i][:movies][j][:worldwide_gro]
+      total += nds[i][:movies][j][:worldwide_gross]
       j += 1 
     end
     i += 1 
   end
-    
+   result 
 
   
   return result
